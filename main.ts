947a337fd10a06409628c17960a5f120e6bd7f9a
LegionPC <john@jjwheatley.com>
2025-06-04 18:52:15 +0200

    Context passing - File text loads, added reference to view for passing context to it

--- main.ts
+++ main.ts
@@ -1,20 +1,24 @@
-import {Menu, Notice, Plugin, TAbstractFile, WorkspaceLeaf} from 'obsidian';
+import {Menu, Plugin, TAbstractFile, WorkspaceLeaf} from 'obsidian';
 import {PanelView} from "./src/classes/panel-view";
 import {VIEW_TYPE} from "./src/constants";
+import {SettingTab} from "./src/classes/settings-tab";
 import {PluginSettings} from "./src/types";
 import {DEFAULT_SETTINGS} from "./src/defaults";
-import {SettingTab} from "./src/classes/settings-tab";
 
 
 export default class OllamaPlugin extends Plugin {
 	settings: PluginSettings;
+	view: PanelView;
 
 	async onload() {
-		await this.loadSettings();
+		await this.loadSettings()
 
 		this.registerView(
 			VIEW_TYPE,
-			(leaf) => new PanelView(leaf, this)
+			(leaf) => {
+				this.view = new PanelView(leaf, this)
+				return this.view
+			}
 		);
 
 		this.addRibbonIcon('brain', 'Ask Ollama', () => {
@@ -24,8 +28,6 @@ export default class OllamaPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
 
-
-
 		// Register menu items to allow passing notes as context
 		this.registerEvent(
 			this.app.workspace.on("file-menu", (menu, file) => {
@@ -34,14 +36,13 @@ export default class OllamaPlugin extends Plugin {
 		);
 
 		this.registerEvent(
-			this.app.workspace.on("editor-menu", (menu, editor, view) => {
-				if(view?.file)
-					this.registerMenuItem(menu, view.file)// Register a view menu item
+			this.app.workspace.on("editor-menu", (menu, _, {file}) => {
+				this.registerMenuItem(menu, file)// Register a view menu item
 			})
 		);
 	}
 
-	registerMenuItem(menu: Menu, file: TAbstractFile){
+	registerMenuItem(menu: Menu, file: TAbstractFile | null){
 		menu.addItem((item) => {
 			item
 				.setTitle("Chat with " + this.settings.aiModal + " about \"" + file?.name.slice(0,-3)+ "\"")
@@ -52,21 +53,29 @@ export default class OllamaPlugin extends Plugin {
 		});
 	}
 
+	async getFileText(contextFilePath: string){
+		const file = this.app.vault.getFileByPath(contextFilePath)
+		return !file ? "" : await this.app.vault.read(file);
+	}
+
 	async activateView(contextFilePath?: string) {
 		const { workspace } = this.app;
 
 		let leaf: WorkspaceLeaf;
 		const leaves = workspace.getLeavesOfType(VIEW_TYPE);
 
+		const context = contextFilePath ? await this.getFileText(contextFilePath) : "";
+		console.log(context);
+
 		if (leaves.length > 0) { // A leaf with our view already exists, use it
 			leaf = leaves[0];
-			await workspace.revealLeaf(leaf);// "Reveal" leaf if sidebar is collapsed
+			await workspace.revealLeaf(leaf);// Expand the sidebar to show leaf if it's collapsed
 		} else { // View isn't found in workspace, create in sidebar as new leaf
 			const rightMostLeaf = workspace.getRightLeaf(false);
 			if(rightMostLeaf) {
 				leaf = rightMostLeaf
 				await leaf.setViewState({type: VIEW_TYPE, active: true});
-				await workspace.revealLeaf(leaf);// "Reveal" leaf if sidebar is collapsed
+				await workspace.revealLeaf(leaf);// Expand the sidebar to show leaf if it's collapsed
 			}
 		}
 	}
