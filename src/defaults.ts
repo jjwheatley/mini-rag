165541aaccfd9088c378befbe35ae063041ce260
LegionPC <john@jjwheatley.com>
2025-06-04 17:43:21 +0200

    Added ToDo

--- src/defaults.ts
+++ src/defaults.ts
@@ -4,3 +4,9 @@ export const DEFAULT_SETTINGS: PluginSettings = {
 	aiModal: '',
 	ollamaURL: 'http://localhost:11434',
 }
+
+// ToDo: Add configurable settings for:
+// - Temperature
+// - top_p
+// - top_k (see: https://ollama.readthedocs.io/en/modelfile/#valid-parameters-and-values)
+
